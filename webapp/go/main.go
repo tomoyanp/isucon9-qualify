c496097a622944458f0b14c58bda1b48856b7c67
tomoyanp <tomoyanpy@gmail.com>
2021-08-16 10:17:01 +0900

    m

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -323,7 +323,7 @@ func init() {
 func main() {
 	host := os.Getenv("MYSQL_HOST")
 	if host == "" {
-		host = "127.0.0.1"
+		host = "172.31.12.242"
 	}
 	port := os.Getenv("MYSQL_PORT")
 	if port == "" {
